a715284f29c7066589018496cb00ac0f9e558d11
amr062739-gif <amr062739@gmail.com>
2026-07-27 22:54:13 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tecnosoft/', // اسم المستودع على GitHub
+  base: '/tecnoplatform2030/', // اسم المستودع على GitHub
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
